88ed34278fc6078c17411fb2fb5517b23c4d01dd
Trey Connelly <treybc@myth55.stanford.edu>
2018-05-02 23:34:51 -0700

    Initial commit of almost working game (can play itself but indexing messed up for humans)

---
+++ game.py
@@ -0,0 +1,130 @@
+from copy import copy
+from random import choice
+import gameUtil
+
+class Game:
+    def __init__(self):
+        self.board = gameUtil.Board()
+        self.tiles1 = gameUtil.tiles1
+        self.tiles2 = gameUtil.tiles2
+
+    #state consists of (board, player 1's hand, player 2's hand, player's turn, isFirstTurn)
+    def getStartState(self):
+        hand1 = [True for i in range(21)]
+        hand2 = [True for i in range(21)]
+        return (self.board.data, hand1, hand2, 1, True)
+
+    #actions consist of (tileID, x_pos, y_pos, rotation_index, reflection_index)
+    def getActions(self, state):
+        data, hand1, hand2, turn, isFirstTurn = state
+        actions = []
+        tiles = self.tiles1
+        hand = hand1
+        if(turn == -1):
+            tiles = self.tiles2
+            hand = hand2
+        for (tileId, inHand) in enumerate(hand):
+            if (inHand):
+                for x in range(self.board.width):
+                    for y in range(self.board.height):
+                        for rotationIndex in range(4):
+                            for reflectionIndex in range(2):
+                                if (self.board.canPlaceTile(tiles[tileId], x, y, rotationIndex, reflectionIndex, data, isFirstTurn)):
+                                    actions.append( (tileId, x, y, rotationIndex, reflectionIndex) )
+        return actions
+
+    #returns the state that follows the given action
+    def getSuccessor(self, state, action):
+        data, hand1, hand2, turn, isFirstTurn = state
+        if (isFirstTurn and (turn==-1)):
+            isFirstTurn = False
+        if (action == 'pass'):
+            return (data, hand1, hand2, -turn, isFirstTurn)
+        tileId, x, y, rotationIndex, reflectionIndex = action
+        if (turn == 1):
+            newHand = copy(hand1)
+            newHand[tileId] = False
+            newData = self.board.getNextBoard(data, self.tiles1[tileId], x, y, rotationIndex, reflectionIndex)
+            return (newData, newHand, hand2, -turn, isFirstTurn)
+        else:
+            newHand = copy(hand2)
+            newHand[tileId] = False
+            newData = self.board.getNextBoard(data, self.tiles2[tileId], x, y, rotationIndex, reflectionIndex)
+            return (newData, hand1, newHand, -turn, isFirstTurn)
+
+    #return whether we have an end state
+    def isEnd(self, state):
+        data, hand1, hand2, turn, isFirstTurn = state
+        nextActions = self.getActions(state)
+        return (self.getActions(state) == []) and (self.getActions( (data,hand1,hand2,-turn,False) ) == [])
+
+    #returns score differential of end state based on blockus rules
+    #5 extra points for ending on single square not yet implemented
+    def getUtility(self, state):
+        data, hand1, hand2, turn, isFirstTurn = state
+        score1 = 0
+        score2 = 0
+        for (tileId, inHand) in enumerate(hand1):
+            if(inHand):
+                score1 -= self.tiles1[tileId].squares
+        for (tileId, inHand) in enumerate(hand2):
+            if(inHand):
+                score2 -= self.tiles2[tileId].squares
+        if(score1 == 0):
+            score1 += 15
+        if(score2 == 0):
+            score2 += 15
+        return score2-score1
+
+class BaselineAgent:
+    def __init__(self, game):
+        self.game = game
+
+    def getAction(self, state):
+        actions = self.game.getActions(state)
+        if (actions == []):
+            return 'pass'
+        for i in range(5,0,-1):
+            parityMoves = [action for action in actions if self.game.tiles1[action[0]].squares == i]
+            if(len(parityMoves) > 0):
+                return choice(parityMoves)
+
+class PlayGame:
+    def __init__(self):
+        self.game = Game()
+        self.agent = BaselineAgent(self.game)
+        self.state = self.game.getStartState()
+
+    def p(self):
+        print(self.game.board)
+
+    def next(self, toPrint = False, action = False):
+        if (action == False):
+            action = self.agent.getAction(self.state)
+        #print(action)
+        self.state = self.game.getSuccessor(self.state, action)
+        self.game.board.data = self.state[0]
+        if(toPrint):
+            self.p()
+        if(self.game.isEnd(self.state)):
+            print("game over. Player 2's net score: " + str(self.game.getUtility(self.state)))
+            print("player 1's tiles remaining: ", [tileId for (tileId, inHand) in enumerate(self.state[1]) if inHand])
+            print("player 2's tiles remaining: ", [tileId for (tileId, inHand) in enumerate(self.state[2]) if inHand])
+            self.p()
+            return False
+        return True
+
+    def simulateGame(self):
+        while(self.next(False)):
+            pass
+
+    def clearGame(self):
+        self.game = Game()
+        self.state = self.game.getStartState()
+
+#simulates 2 games
+#currently aggressively unoptimized, so takes a few seconds to play out a whole game
+p = PlayGame()
+#p.simulateGame()
+#p.clearGame()
+#p.simulateGame()
---
+++ gameUtil.py
@@ -0,0 +1,290 @@
+from copy import deepcopy
+
+class Board:
+    #class to hold game board and pieces
+    # a 0 represents an empty square
+    # 1 represents a square belonging to player 1, and 2 a square belonging to player 2
+    # 3 for special start position
+    
+    def __init__(self):
+        self.width = 14
+        self.height = 14
+        #initialize 14x14 grid of points
+        self.data =[[0 for y in range(self.height)] for x in range(self.width)]
+        self.data[4][4] = 3
+        self.data[9][9] = 3
+        self.numPiecesPlayed = 0
+
+    #str method basically copied from game.py in pacman assignment
+    def __str__(self):
+
+        #out = [[str(self.data[y][x])[0] for y in range(self.height)] for x in range(self.width)]
+        #out.reverse()
+        out = []
+        for r in range(self.height):
+            x = []
+            for c in range(self.width):
+                if(self.data[r][c] == 0):
+                    x.append('.')
+                else:
+                    x.append(str(self.data[r][c]))
+            out.append(x)
+        out.reverse()
+        return '\n'.join([''.join(z) for z in out])
+        #return '\n'.join([' '.join(x) for x in out])
+
+    #method for accessing data where key is (x,y) tuble
+    def __getitem__(self,key):
+        x,y = key
+        if(x<0 or x>=self.width or y<0 or y>= self.height):
+            return -1
+        else:
+            return self.data[x][y]
+
+    def get(self,x,y,data):
+        if(x<0 or x>=self.width or y<0 or y>= self.height):
+            return -1
+        else:
+            return data[x][y]
+    
+    #set data using (x,y) tuple (for consistency with Tile)
+    def __setitem__(self,key,item):
+        x,y = key
+        self.data[x][y] = item
+
+    #like placeTile but returns a new board instead of updating the original board
+    #assumes that tile placement is valid
+    def getNextBoard(self, data, tile, x, y, rot = 0, ref = 0):
+        nextBoard = deepcopy(data)
+        tile.transform(rot,ref)
+        for r in range(1,tile.tileHeight+1):
+            for c in range(1,tile.tileWidth+1):
+                if(tile[(r,c)] == 'P'):
+                    nextBoard[x+c-1][y+r-1] = tile.player
+        return nextBoard
+
+    #add a tile to the Board (assumes that tile can be placed)
+    #tile: a Tile object representing tile to be places
+    #x,y: position on the board to place
+    #rot: 90 degree multiple for rotation, ref: wheteher to reflect
+    #safe: if true, make sure tile placement is legal first
+    #p: if true, print grid after placement
+    def placeTile(self, tile, x, y, rot = 0, ref = 0, safe = False, p = False):
+        if(safe):
+            if (not self.canPlaceTile(tile,x,y,rot,ref)):
+                return False
+        else:
+            tile.transform(rot,ref)
+        for r in range(1,tile.tileHeight+1):
+            for c in range(1,tile.tileWidth+1):
+                if(tile[(r,c)] == 'P'):
+                    self[(x+c-1,y+r-1)] = tile.player
+        self.numPiecesPlayed += 1
+        if(p):
+            print(self)
+        return True
+
+
+    #check whether a specific tile can be placed in the grid at this position
+    #(x,y) are the coordinates of the bottom-left bounding box of the piece
+    def canPlaceTile(self, tile, x, y, rot = 0, ref = 0, data = False, isFirstTurn = False):
+        if (data == False):
+            data = self.data
+        if (not tile.transform(rot,ref)):
+            return False
+        tileWidth = tile.tileWidth
+        tileHeight = tile.tileHeight
+        if((tileWidth+x > self.width) or (tileHeight+y > self.height)):
+            return False
+        numCorners = 0
+        if(not isFirstTurn):
+            for r in range(tileHeight+2):
+                for c in range(tileWidth+2):
+                    if(tile[(r,c)] == 'P' and self.get(x+c-1,y+r-1,data) != 0):
+                        return False
+                    elif(tile[(r,c)] == 'N' and self.get(x+c-1,y+r-1,data) == tile.player):
+                        return False
+                    elif(tile[(r,c)] == 'C' and self.get(x+c-1,y+r-1,data) == tile.player):
+                        numCorners+=1
+        else:
+            for r in range(1,tileHeight+1):
+                for c in range(1,tileWidth+1):
+                    if(tile[(r,c)] == 'P' and self.get(x+c-1,y+r-1,data) == 3):
+                        numCorners+=1
+        return (numCorners > 0)
+
+
+
+class Tile:
+    #class to represent a single tile under rotation
+    #initialized from data grid files
+    #note that data is a (tileWidth+2)x(tileHeight+2) array
+    #symmetry level: 0 = no distinct transformations (1)
+    #   1 = distinct under 90 degree rotation clockwise only (2)
+    #   2 = distinct under 90 degree rotation clockwise and/or reflection (4)
+    #   3 = distinct under 90, 180, 270 degree rotation clockwise, and reflection (8)
+    def __init__(self, tileWidth, tileHeight, dataArray, symmetryLevel, player, squares):
+        self.data = dataArray
+        self.tileWidth = tileWidth
+        self.tileHeight = tileHeight
+        self.symmetryLevel = symmetryLevel
+        self.rotation = 0
+        self.reflection = 0
+        self.player = player
+        self.squares = squares
+
+    def __str__(self):
+        out = []
+        for r in range(self.tileHeight+2):
+            x = []
+            for c in range(self.tileWidth+2):
+                if(self[(r,c)] == 'P'):
+                    x.append('@')
+                else:
+                    x.append(' ')
+            out.append(x)
+        out.reverse()
+        return '\n'.join([''.join(z) for z in out])
+
+    def transform(self, rot, ref, p = False):
+        rotated = self.rotate(rot)
+        reflected = True
+        if (self.reflection != ref):
+            reflected = self.reflect()
+        if(p):
+            print(self)
+        return (rotated and reflected)
+    
+    def reflect(self):
+        if(self.symmetryLevel <2):
+            return False
+        self.reflection = (self.reflection+1)%2
+        return True
+
+    def rotate(self, i):
+        if(self.symmetryLevel == 0):
+            return (i == 0)
+        elif((i > 1) and (self.symmetryLevel < 3)):
+            return False
+        elif((i%2)!=(self.rotation%2)):
+            temp = self.tileWidth
+            self.tileWidth = self.tileHeight
+            self.tileHeight = temp
+        self.rotation = i
+        return True
+
+    #takes a tuple of coordinates and returns the corresponding rotated/reflected value
+    def __getitem__(self, key):
+        #rotation not yet implemented
+        y,x = key
+        if(self.reflection==1):
+            x = self.tileWidth+1-x
+        if(self.rotation==1):
+            temp = self.tileHeight+1-y
+            y = x
+            x = temp
+        elif(self.rotation==2):
+            x = self.tileWidth+1-x
+            y = self.tileHeight+1-y
+        elif(self.rotation==3):
+            temp = self.tileWidth+1-x
+            x = y
+            y = temp
+        return self.data[y][x]
+
+
+
+#polyomino data (each data array is official piece name; o and b stand for orange and blue
+board = Board()
+#Tile(width,height,name,symmetry,player)
+i1 = [['C','N','C'],['N','P','N'],['C','N','C']]
+o1 = Tile(1,1,i1,0,1,1)
+b1 = Tile(1,1,i1,0,2,1)
+
+i2 = ['CNC','NPN','NPN','CNC']
+o2 = Tile(1,2,i2,1,1,2)
+b2 = Tile(1,2,i2,1,2,2)
+
+i3 = ['CNC','NPN','NPN','NPN','CNC']
+o3 = Tile(1,3,i3,1,1,3)
+b3 = Tile(1,3,i3,1,2,3)
+
+crooked3 = ['CNC.','NPNC','NPPN','CNNC']
+o4 = Tile(2,2,crooked3,2,1,3)
+b4 = Tile(2,2,crooked3,2,2,3)
+
+i4 = ['CNC','NPN','NPN','NPN','NPN','CNC']
+o5 = Tile(1,4,i4,1,1,4)
+b5 = Tile(1,4,i4,1,2,4)
+
+shortL = ['.CNC','.NPN','CNPN','NPPN','CNNC']
+o6 = Tile(2,3,shortL,3,1,4)
+b6 = Tile(2,3,shortL,3,2,4)
+
+shortT = ['CNC.','NPNC','NPPN','NPNC','CNCP']
+o7 = Tile(2,3,shortT,2,1,4)
+b7 = Tile(2,3,shortT,2,2,4)
+
+square = ['CNNC','NPPN','NPPN','CNNC']
+o8 = Tile(2,2,square,0,1,4)
+b8 = Tile(2,2,square,0,2,4)
+
+shortZ = ['CNNC.','NPPNC','CNPPN','.CNNC']
+o9 = Tile(3,2,shortZ,2,1,4)
+b9 = Tile(3,2,shortZ,2,2,4)
+
+i5 = ['CNC','NPN','NPN','NPN','NPN','NPN','CNC']
+o10 = Tile(1,5,i5,1,1,5)
+b10 = Tile(1,5,i5,1,2,5)
+
+l = ['.CNC','.NPN','.NPN','CNPN','NPPN','CNNC']
+o11 = Tile(2,4,l,3,1,5)
+b11 = Tile(2,4,l,3,2,5)
+
+n = ['.CNC','.NPN','CNPN','NPPN','NPNC','CNC.']
+o12 = Tile(2,4,n,3,1,5)
+b12 = Tile(2,4,n,3,2,5)
+
+p = ['.CNC','CNPN','NPPN','NPPN','CNNC']
+o13 = Tile(2,3,p,3,1,5)
+b13 = Tile(2,3,p,3,2,5)
+
+u = ['CNNC','NPPN','CNPN','NPPN','CNNC']
+o14 = Tile(2,3,u,2,1,5)
+b14 = Tile(2,3,u,2,2,5)
+
+y = ['CNC.','NPNC','NPPN','NPNC','NPN.','CNC.']
+o15 = Tile(2,4,y,3,1,5)
+b15 = Tile(2,4,y,3,2,5)
+
+t = ['CNC..','NPNNC','NPPPN','NPNNC','CNC..']
+o16 = Tile(3,3,t,2,1,5)
+b16 = Tile(3,3,t,2,2,5)
+
+v = ['CNC..','NPN..','NPNNC','NPPPN','CNNNC']
+o17 = Tile(3,3,v,2,1,5)
+b17 = Tile(3,3,v,2,2,5)
+
+w = ['CNNC.','NPPNC','CNPPN','.CNPN','..CNC']
+o18 = Tile(3,3,w,2,1,5)
+b18 = Tile(3,3,w,2,2,5)
+
+z = ['CNC..','NPNNC','NPPPN','CNNPN','..CNC']
+o19 = Tile(3,3,z,2,1,5)
+b19 = Tile(3,3,z,2,2,5)
+
+f = ['CNC..','NPNNC','NPPPN','CNPNC','.CNC.']
+o20 = Tile(3,3,f,3,1,5)
+b20 = Tile(3,3,f,3,2,5)
+
+x = ['.CNC.','CNPNC','NPPPN','CNPNC','.CNC.']
+o21 = Tile(3,3,x,0,1,5)
+b21 = Tile(3,3,x,0,2,5)
+
+#build the player's hands
+tiles1 = [o1,o2,o3,o4,o5,o6,o7,o8,o9,o10,o11,o12,o13,o14,o15,o16,o17,o18,o19,o20,o21]
+tiles2 = [b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,b11,b12,b13,b14,b15,b16,b17,b18,b19,b20,b21]
+
+
+#board.placeTile(b15,0,9,0,0)
+print(board)
